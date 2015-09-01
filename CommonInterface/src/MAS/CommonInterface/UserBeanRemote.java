b455da8a51d0172e430abc3679b40bd6eb2314b3
Jonathan Lau <jon@jon.sg>
2015-09-01 10:56:30 +0800

    Add @Remote annotation to CommonInterface

--- CommonInterface/src/MAS/CommonInterface/UserBeanRemote.java
+++ CommonInterface/src/MAS/CommonInterface/UserBeanRemote.java
@@ -1,8 +1,8 @@
 package MAS.CommonInterface;
 
-/**
- * Created by Jonathan on 25/8/2015.
- */
+import javax.ejb.Remote;
+
+@Remote
 public interface UserBeanRemote {
     void createUser(String username, String password, String firstName, String lastName, String email);
 }
