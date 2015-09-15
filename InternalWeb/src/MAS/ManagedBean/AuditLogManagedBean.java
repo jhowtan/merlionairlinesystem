c0347f842b4af13c05c812f46e7187b13107252d
Jonathan Lau <jon@jon.sg>
2015-09-16 01:57:26 +0800

    Implement exporting of audit logs to CSV.

--- InternalWeb/src/MAS/ManagedBean/AuditLogManagedBean.java
+++ InternalWeb/src/MAS/ManagedBean/AuditLogManagedBean.java
@@ -2,10 +2,15 @@ package MAS.ManagedBean;
 
 import MAS.Bean.AuditLogBean;
 import MAS.Entity.AuditLog;
-import MAS.Exception.NotFoundException;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @ManagedBean
@@ -15,6 +20,41 @@ public class AuditLogManagedBean {
 
     public List<AuditLog> getAllAuditLogs() {
         return auditLogBean.getAllAuditLogs();
+    }
+
+    public void exportToCSV() {
+        CSVFormat csvFormat = CSVFormat.DEFAULT.withRecordSeparator("\n");
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            CSVPrinter csvPrinter = new CSVPrinter(sb, csvFormat);
+            csvPrinter.printRecord(new String[] {"Timestamp", "Username", "Category", "Log Entry"});
+            ArrayList<String> record;
+            for (AuditLog auditLog : auditLogBean.getAllAuditLogs()) {
+                record = new ArrayList<>();
+                record.add(auditLog.getTimestamp().toString());
+                record.add(auditLog.getUser().getUsername());
+                record.add(auditLog.getCategory());
+                record.add(auditLog.getDescription());
+                csvPrinter.printRecord(record);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        HttpServletResponse response = (HttpServletResponse) ctx.getExternalContext().getResponse();
+        response.setHeader("Content-disposition", "attachment; filename=export.csv");
+        response.setContentLength(sb.length());
+        response.setContentType("text/csv");
+        try {
+            response.getOutputStream().write(sb.toString().getBytes());
+            response.getOutputStream().flush();
+            response.getOutputStream().close();
+            ctx.responseComplete();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
