f54237f4a0e4e2333642147e4478ff6687e6483e
Jonathan Tan <jhow.tan@gmail.com>
2015-09-17 16:57:32 +0800

    Start implementation of edit workgroup methods: edit workgroup details, change workgroup owner

--- Common/src/MAS/Bean/WorkgroupBean.java
+++ Common/src/MAS/Bean/WorkgroupBean.java
@@ -44,10 +44,53 @@ public class WorkgroupBean {
         return workgroup.getId();
     }
 
+    public long editWorkgroup(long wgId, String name, String description, List<Long> userIds) throws NotFoundException {
+        Workgroup workgroup = em.find(Workgroup.class, wgId);
+        workgroup.setName(name);
+        workgroup.setDescription(description);
+        User user;
+        ArrayList<User> users = new ArrayList<>();
+        for(Long userId : userIds) {
+            user = em.find(User.class, userId);
+            if (user != null) {
+                users.add(user);
+            }
+        }
+        workgroup.setUsers(users);
+        em.persist(workgroup);
+        em.flush();
+        return workgroup.getId();
+    }
+
+    public long changeWorkgroupOwner(long wgId, long newOwnerId) throws NotFoundException {
+        Workgroup workgroup = em.find(Workgroup.class, wgId);
+        if (workgroup == null) throw new NotFoundException();
+
+        User newOwner = em.find(User.class, newOwnerId);
+        if (newOwner == null) throw new NotFoundException();
+
+        if (workgroup.getUsers().contains(newOwner)) {
+            workgroup.setOwner(newOwner);
+            em.persist(workgroup);
+            return newOwnerId;
+        }
+        else {
+            throw new NotFoundException("New owner of the workgroup is not part of the workgroup.");
+        }
+    }
+
     public void removeWorkgroup(long id) throws NotFoundException {
         Workgroup workgroup = em.find(Workgroup.class, id);
         if (workgroup == null) throw new NotFoundException();
         em.remove(workgroup);
+    }
+
+    public void removeUserFromWorkgroup(long userId, long wgId) throws NotFoundException {
+        Workgroup workgroup = em.find(Workgroup.class, wgId);
+        User user = em.find(User.class, userId);
+        if (workgroup.getUsers().remove(user)) {
+            em.persist(workgroup);
+        }
     }
 
     public boolean isNameUnique(String name) {
