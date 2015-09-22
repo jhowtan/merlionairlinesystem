4ae05a68dd7f3726fff841a8898b4273afd5c5cb
Jonathan Tan <jhow.tan@gmail.com>
2015-09-23 00:43:58 +0800

    Implement Create Aircraft Type route and provide validation

---
+++ InternalWeb/src/MAS/ManagedBean/CreateAircraftTypeManagedBean.java
@@ -0,0 +1,52 @@
+package MAS.ManagedBean;
+
+import MAS.Bean.FleetBean;
+
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+public class CreateAircraftTypeManagedBean {
+    @EJB
+    FleetBean fleetBean;
+
+    @ManagedProperty(value="#{authManagedBean}")
+    private AuthManagedBean authManagedBean;
+
+    private String typeName;
+    private int fuelCapacity;
+
+    public void createType() {
+        fleetBean.createAircraftType(typeName, fuelCapacity);
+        authManagedBean.createAuditLog("Created new aircraft type: " + typeName, "create_aircraft_type");
+        setTypeName(null);
+        setFuelCapacity(0);
+        FacesMessage m = new FacesMessage("Aircraft Type created successfully.");
+        m.setSeverity(FacesMessage.SEVERITY_INFO);
+        FacesContext.getCurrentInstance().addMessage("status", m);
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(int fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public void setAuthManagedBean(AuthManagedBean authManagedBean) {
+        this.authManagedBean = authManagedBean;
+    }
+
+}
