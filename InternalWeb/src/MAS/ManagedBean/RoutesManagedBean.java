27a799e0819497109f6f4e98447e6f1f8e8aa705
Jonathan Lau <jon@jon.sg>
2015-09-24 18:57:39 +0800

    Add route visualization map.

--- InternalWeb/src/MAS/ManagedBean/RoutesManagedBean.java
+++ InternalWeb/src/MAS/ManagedBean/RoutesManagedBean.java
@@ -3,12 +3,16 @@ package MAS.ManagedBean;
 import MAS.Bean.RouteBean;
 import MAS.Entity.Route;
 import MAS.Exception.NotFoundException;
+import com.google.gson.Gson;
 
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @ManagedBean
@@ -44,4 +48,61 @@ public class RoutesManagedBean {
     public void setAuthManagedBean(AuthManagedBean authManagedBean) {
         this.authManagedBean = authManagedBean;
     }
+
+
+    private class MapRoute {
+        public String origin;
+        public String destination;
+        public double originLat;
+        public double originLon;
+        public double destinationLat;
+        public double destinationLon;
+
+        @Override
+        public boolean equals(Object obj) {
+            return obj instanceof MapRoute && (
+                    (this.origin.equals(((MapRoute) obj).origin) && this.destination.equals(((MapRoute) obj).destination)) ||
+                    (this.origin.equals(((MapRoute) obj).destination) && this.destination.equals(((MapRoute) obj).origin))
+            );
+        }
+    }
+
+    public void mapRoutesJson() {
+        ArrayList<MapRoute> mapRoutes = new ArrayList<>();
+        for (Route route : routeBean.getAllRoutes()) {
+            MapRoute r = new MapRoute();
+            r.origin = route.getOrigin().getCode();
+            r.destination = route.getDestination().getCode();
+            r.originLat = route.getOrigin().getLatitude();
+            r.originLon = route.getOrigin().getLongitude();
+            r.destinationLat = route.getDestination().getLatitude();
+            r.destinationLon = route.getDestination().getLongitude();
+            if(!mapRoutes.contains(r)) {
+                mapRoutes.add(r);
+            }
+        }
+
+        Gson gson = new Gson();
+        String json = gson.toJson(mapRoutes);
+
+        if(!authManagedBean.isAuthenticated()) {
+            json = "[]";
+        }
+
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        HttpServletResponse response = (HttpServletResponse) ctx.getExternalContext().getResponse();
+        response.setContentLength(json.length());
+        response.setContentType("application/json");
+
+        try {
+            response.getOutputStream().write(json.getBytes());
+            response.getOutputStream().flush();
+            response.getOutputStream().close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ctx.responseComplete();
+    }
+
 }
