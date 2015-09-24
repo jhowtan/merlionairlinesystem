b5435a501fb902dfef3a9f74bdf73ae2ce52ad2e
Jonathan Tan <jhow.tan@gmail.com>
2015-09-24 12:41:46 +0800

    Implemented infö messages and delete functionality with safeguards

--- InternalWeb/src/MAS/ManagedBean/CityManagedBean.java
+++ InternalWeb/src/MAS/ManagedBean/CityManagedBean.java
@@ -1,13 +1,13 @@
 package MAS.ManagedBean;
 
-import MAS.Bean.FleetBean;
 import MAS.Bean.RouteBean;
 import MAS.Entity.City;
-import MAS.Exception.NotFoundException;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
 import java.util.List;
 
 @ManagedBean
@@ -24,14 +24,22 @@ public class CityManagedBean {
 
     public void delete(long id) {
         try {
+            String cityName = routeBean.getCity(id).getName();
             routeBean.removeCity(id);
-            authManagedBean.createAuditLog("Deleted city: " + routeBean.getCity(id).getName(), "delete_city");
-        } catch (NotFoundException e) {
+            authManagedBean.createAuditLog("Deleted city: " + cityName, "delete_city");
+            FacesMessage m = new FacesMessage("Successfully deleted the city: " + cityName);
+            m.setSeverity(FacesMessage.SEVERITY_INFO);
+            FacesContext.getCurrentInstance().addMessage("status", m);
+        } catch (Exception e) {
             e.printStackTrace();
+            FacesMessage m = new FacesMessage("Unable to delete city, please check if there are existing airports created for this city.");
+            m.setSeverity(FacesMessage.SEVERITY_INFO);
+            FacesContext.getCurrentInstance().addMessage("status", m);
         }
     }
 
     public void setAuthManagedBean(AuthManagedBean authManagedBean) {
         this.authManagedBean = authManagedBean;
     }
+
 }
--- InternalWeb/src/MAS/ManagedBean/CountryManagedBean.java
+++ InternalWeb/src/MAS/ManagedBean/CountryManagedBean.java
@@ -1,13 +1,13 @@
 package MAS.ManagedBean;
 
-import MAS.Bean.FleetBean;
 import MAS.Bean.RouteBean;
 import MAS.Entity.Country;
-import MAS.Exception.NotFoundException;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
 import java.util.List;
 
 @ManagedBean
@@ -24,10 +24,17 @@ public class CountryManagedBean {
 
     public void delete(long id) {
         try {
+            String countryName = routeBean.getCountry(id).getName();
             routeBean.removeCountry(id);
-            authManagedBean.createAuditLog("Deleted country: " + routeBean.getCountry(id).getName(), "delete_country");
-        } catch (NotFoundException e) {
+            authManagedBean.createAuditLog("Deleted country: " + countryName, "delete_country");
+            FacesMessage m = new FacesMessage("Successfully deleted the country: " + countryName);
+            m.setSeverity(FacesMessage.SEVERITY_INFO);
+            FacesContext.getCurrentInstance().addMessage("status", m);
+        } catch (Exception e) {
             e.printStackTrace();
+            FacesMessage m = new FacesMessage("Unable to delete country, please check if there are existing cities created for this country.");
+            m.setSeverity(FacesMessage.SEVERITY_INFO);
+            FacesContext.getCurrentInstance().addMessage("status", m);
         }
     }
 
