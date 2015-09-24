0f5aced9c7805c0fab0498929bf8d81703bba014
sofox <darylhzw@gmail.com>
2015-09-24 15:45:07 +0800

    Seed database

--- Common/src/MAS/Bean/InitBean.java
+++ Common/src/MAS/Bean/InitBean.java
@@ -11,6 +11,7 @@ import javax.ejb.Startup;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @Singleton
@@ -20,6 +21,10 @@ public class InitBean {
     RoleBean roleBean;
     @EJB
     UserBean userBean;
+    @EJB
+    FleetBean fleetBean;
+    @EJB
+    RouteBean routeBean;
 
     @PostConstruct
     public void init() {
@@ -39,6 +44,32 @@ public class InitBean {
                 userBean.setRoles(userId, Arrays.asList(roleId));
             } catch (NotFoundException e) {
                 e.printStackTrace();
+            }
+        }
+
+        if (fleetBean.getAllAircraftTypes().size() == 0) {
+            try {
+                long acTypeId = fleetBean.createAircraftType("A380 ABC", 4000);
+                long seatConfId = fleetBean.createAircraftSeatConfig("ss|sss|ss/ss|sss|ss/ss|sss|ss/_e", "A3180 ABC test", 5800, acTypeId);
+                long acId = fleetBean.createAircraft("9V-ABC", new Date());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (routeBean.getAllAirports().size() == 0) {
+            try {
+                long ctryId = routeBean.createCountry("Singapore", "SGP");
+                long ctId = routeBean.createCity("Singapore", ctryId);
+                long apId = routeBean.createAirport("Changi Airport", 1.3644202, 103.9915308, "SIN", 3, ctId);
+
+                long ctry2Id = routeBean.createCountry("United Kingdom", "GBR");
+                long ct2Id = routeBean.createCity("London", ctry2Id);
+                long ap2Id = routeBean.createAirport("Heathrow Airport", 51.4700223, -0.4542955, "LHR", 4, ct2Id);
+
+                routeBean.createRoute(apId, ap2Id);
+            } catch (Exception e) {
+
             }
         }
     }
