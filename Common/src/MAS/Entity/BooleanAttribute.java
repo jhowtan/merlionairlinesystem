c4d02212d7a354e1e5c0013cc9d42f100c66bfb0
Jonathan Lau <jon@jon.sg>
2015-10-10 23:45:00 +0800

    Create BooleanAttribute.

---
+++ Common/src/MAS/Entity/BooleanAttribute.java
@@ -0,0 +1,30 @@
+package MAS.Entity;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class BooleanAttribute {
+    private String key;
+
+    @Id
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    private boolean value;
+
+    @Basic
+    public boolean getValue() {
+        return value;
+    }
+
+    public void setValue(boolean value) {
+        this.value = value;
+    }
+}
