5a5723fc79d1e202fb1832d7898bde8204701366
sofox <darylhzw@gmail.com>
2015-10-19 13:16:13 +0800

    Improve UI for schedule dev

--- InternalWeb/src/MAS/ManagedBean/ScheduleDev/scheduleDevManagedBean.java
+++ InternalWeb/src/MAS/ManagedBean/ScheduleDev/scheduleDevManagedBean.java
@@ -3,6 +3,7 @@ package MAS.ManagedBean.ScheduleDev;
 import MAS.Bean.FleetBean;
 import MAS.Bean.RouteBean;
 import MAS.Bean.ScheduleDevelopmentBean;
+import MAS.Entity.Aircraft;
 import MAS.Entity.Airport;
 import MAS.Exception.NotFoundException;
 
@@ -31,15 +32,21 @@ public class scheduleDevManagedBean {
     private List<Airport> selectAirports;
     private List<String> hubAirportsId;
     private List<Airport> hubAirports;
+
+    private List<Aircraft> allAircrafts;
+    private List<Long> selectAircraftsId;
+    private List<Aircraft> selectAircrafts;
     private int step = 0;
 
     @PostConstruct
     private void init() {
         setAllAirports(routeBean.getAllAirports());
+        setAllAircrafts(fleetBean.getAllAircraft());
     }
 
     public void selectAirportAjaxListener(AjaxBehaviorEvent event) {
         selectAirports = new ArrayList<>();
+        hubAirports = new ArrayList<>();
         for (int i = 0; i < selectAirportsId.size(); i++) {
             try {
                 selectAirports.add(routeBean.getAirport(selectAirportsId.get(i)));
@@ -72,14 +79,25 @@ public class scheduleDevManagedBean {
         return false;
     }
 
+    public void saveAirports() {
+        //Save hub str
+        step = 1;
+    }
+
     public boolean displayAc(){
         if (step == 1) return true;
         return false;
     }
 
-    public void saveAirports() {
-        //Save hub str
-        step = 1;
+    public void selectAircraftsAjaxListener(AjaxBehaviorEvent event) {
+        selectAircrafts = new ArrayList<>();
+        for (int i = 0; i < selectAircraftsId.size(); i++) {
+            try {
+                selectAircrafts.add(fleetBean.getAircraft(selectAircraftsId.get(i)));
+            } catch (NotFoundException e) {
+                //Aircraft not found
+            }
+        }
     }
 
     public List<Airport> getAllAirports() {
@@ -128,5 +146,29 @@ public class scheduleDevManagedBean {
 
     public void setHubAirportsId(List<String> hubAirportsId) {
         this.hubAirportsId = hubAirportsId;
+    }
+
+    public List<Aircraft> getAllAircrafts() {
+        return allAircrafts;
+    }
+
+    public void setAllAircrafts(List<Aircraft> allAircrafts) {
+        this.allAircrafts = allAircrafts;
+    }
+
+    public List<Long> getSelectAircraftsId() {
+        return selectAircraftsId;
+    }
+
+    public void setSelectAircraftsId(List<Long> selectAircraftsId) {
+        this.selectAircraftsId = selectAircraftsId;
+    }
+
+    public List<Aircraft> getSelectAircrafts() {
+        return selectAircrafts;
+    }
+
+    public void setSelectAircrafts(List<Aircraft> selectAircrafts) {
+        this.selectAircrafts = selectAircrafts;
     }
 }
