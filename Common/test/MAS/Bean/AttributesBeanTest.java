f021f28c6d7a80dd0caa5b12a7bd8eceb7ad6962
Jonathan Lau <jon@jon.sg>
2015-10-21 04:24:39 +0800

    Use OpenEJB for testing.

--- Common/test/MAS/Bean/AttributesBeanTest.java
+++ Common/test/MAS/Bean/AttributesBeanTest.java
@@ -4,13 +4,28 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.ejb.embeddable.EJBContainer;
+
+import java.util.Properties;
+
 import static org.junit.Assert.*;
 
 public class AttributesBeanTest {
 
     @Before
     public void setUp() throws Exception {
-
+        Properties p = new Properties();
+        p.put("em", "new://Resource?type=DataSource");
+        p.put("em.JdbcDriver", "com.mysql.jdbc.Driver");
+        p.put("em.JdbcUrl", "jdbc:mysql://localhost/mastest");
+        p.put("em.UserName", "root");
+        p.put("em.Password", "admin");
+        p.put("em.Provider", "org.eclipse.persistence.jpa.PersistenceProvider");
+//        scmaccess-unit.eclipselink.jdbc.batch-writing=JDBC
+//        scmaccess-unit.eclipselink.target-database=Auto
+//        scmaccess-unit.eclipselink.ddl-generation=drop-and-create-tables
+//        scmaccess-unit.eclipselink.ddl-generation.output-mode=database
+        EJBContainer.createEJBContainer(p);
     }
 
     @After
