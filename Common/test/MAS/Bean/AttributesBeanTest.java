f99a7659e48ee76ee088db77605a3a68311729f3
Jonathan Lau <jon@jon.sg>
2015-10-21 03:09:20 +0800

    Scaffold JUnit testing.

---
+++ Common/test/MAS/Bean/AttributesBeanTest.java
@@ -0,0 +1,30 @@
+package MAS.Bean;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AttributesBeanTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStringAttribute() throws Exception {
+
+    }
+
+    @Test
+    public void testSetStringAttribute() throws Exception {
+
+    }
+}
