08f597744a51d3322732495233f1e76b8ec5b384
Jonathan Tan <jhow.tan@gmail.com>
2015-10-22 12:35:44 +0800

    Changed facesMessage css formating for display of system messages

--- InternalWeb/src/MAS/ManagedBean/PriceManagement/FareRuleManagedBean.java
+++ InternalWeb/src/MAS/ManagedBean/PriceManagement/FareRuleManagedBean.java
@@ -6,6 +6,7 @@ import MAS.Exception.NotFoundException;
 import MAS.ManagedBean.Auth.AuthManagedBean;
 
 import javax.ejb.EJB;
+import javax.ejb.EJBException;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
@@ -33,6 +34,11 @@ public class FareRuleManagedBean {
             e.getMessage();
             FacesMessage m = new FacesMessage("The fare rule cannot be found, or may have already been deleted.");
             m.setSeverity(FacesMessage.SEVERITY_INFO);
+            FacesContext.getCurrentInstance().addMessage("status", m);
+        } catch (EJBException e) {
+            e.getMessage();
+            FacesMessage m = new FacesMessage("The fare rule you are trying to delete is in use by existing flights in operation.");
+            m.setSeverity(FacesMessage.SEVERITY_ERROR);
             FacesContext.getCurrentInstance().addMessage("status", m);
         }
     }
