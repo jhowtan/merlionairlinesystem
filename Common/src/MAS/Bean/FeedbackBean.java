1c353b814ebda9cf575e3c8a5196554ba426e21a
sofox <darylhzw@gmail.com>
2015-11-11 00:45:10 +0800

    Add Feedback entity and bean

---
+++ Common/src/MAS/Bean/FeedbackBean.java
@@ -0,0 +1,13 @@
+package MAS.Bean;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+@Stateless(name = "FeedbackEJB")
+@LocalBean
+public class FeedbackBean {
+    public FeedbackBean() {
+    }
+
+
+}
---
+++ Common/src/MAS/Entity/Feedback.java
@@ -0,0 +1,76 @@
+package MAS.Entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by Daryl on 10/11/2015.
+ */
+@Entity
+public class Feedback {
+    private long id;
+
+    @GeneratedValue
+    @Id
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    private String email;
+
+    @Basic
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String subject;
+
+    @Basic
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    private String body;
+
+    @Basic
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    private boolean responded;
+
+    @Basic
+    public boolean isResponded() {
+        return responded;
+    }
+
+    public void setResponded(boolean responded) {
+        this.responded = responded;
+    }
+
+    private Customer customer;
+
+    @ManyToOne
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
