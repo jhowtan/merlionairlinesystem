20f68adae63ebced912081ba120c5ec088c42b7f
Jonathan Tan <jhow.tan@gmail.com>
2015-11-12 02:24:33 +0800

    Improve crew management reporting

--- InternalWeb/src/MAS/ManagedBean/ManagementReporting/CrewReportManagedBean.java
+++ InternalWeb/src/MAS/ManagedBean/ManagementReporting/CrewReportManagedBean.java
@@ -3,6 +3,7 @@ package MAS.ManagedBean.ManagementReporting;
 import MAS.Bean.AircraftMaintenanceSlotBean;
 import MAS.Bean.FlightRosterBean;
 import MAS.Bean.UserBean;
+import MAS.Entity.User;
 import MAS.ManagedBean.Auth.AuthManagedBean;
 
 import javax.annotation.PostConstruct;
@@ -27,9 +28,11 @@ public class CrewReportManagedBean {
     private List<String> jobs;
     private List<String> jobInputs;
     private String jobIds;
+    private List<User> crewList;
 
     @PostConstruct
     public void init() {
+        crewList = new ArrayList<>();
         jobs = new ArrayList<>();
         jobs.add("Cabin Crew");
         jobs.add("Cockpit Crew");
@@ -39,19 +42,36 @@ public class CrewReportManagedBean {
     public void filter() {
         jobIds = "";
         List<Integer> jobsConverted = new ArrayList<>();
+        crewList = new ArrayList<>();
         for (int i=0; i < jobInputs.size(); i++) {
             if (jobInputs.get(i).equals("Cabin Crew")) {
                 jobsConverted.add(3);
+                crewList.addAll(userBean.getUsersWithJobs(3));
             }
             else if (jobInputs.get(i).equals("Cockpit Crew")) {
                 jobsConverted.add(4);
+                crewList.addAll(userBean.getUsersWithJobs(4));
             }
             else if (jobInputs.get(i).equals("Maintenance Crew")) {
                 jobsConverted.add(5);
+                crewList.addAll(userBean.getUsersWithJobs(5));
             }
         }
         for (int i = 0; i < jobsConverted.size(); i++) {
             jobIds = jobIds.concat(jobsConverted.get(i).toString()).concat("-");
+        }
+    }
+
+    public String getJobString(int job) {
+        switch(job) {
+            case 3:
+                return "Cabin Crew";
+            case 4:
+                return "Cockpit Crew";
+            case 5:
+                return "Maintenance Crew";
+            default:
+                return "";
         }
     }
 
@@ -85,5 +105,13 @@ public class CrewReportManagedBean {
 
     public void setJobIds(String jobIds) {
         this.jobIds = jobIds;
+    }
+
+    public List<User> getCrewList() {
+        return crewList;
+    }
+
+    public void setCrewList(List<User> crewList) {
+        this.crewList = crewList;
     }
 }
