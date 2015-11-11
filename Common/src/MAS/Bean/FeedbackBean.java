8a3e76dd4f3f25c5c7d5b30bff4ecde3908ae0a7
Jonathan Tan <jhow.tan@gmail.com>
2015-11-12 07:53:46 +0800

    Create backend methods for data wrangling of feedback

--- Common/src/MAS/Bean/FeedbackBean.java
+++ Common/src/MAS/Bean/FeedbackBean.java
@@ -1,11 +1,13 @@
 package MAS.Bean;
 
 import MAS.Entity.Feedback;
+import MAS.Exception.NotFoundException;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Stateless(name = "FeedbackEJB")
 @LocalBean
@@ -20,5 +22,23 @@ public class FeedbackBean {
         em.persist(feedback);
         em.flush();
         return feedback;
+    }
+
+    public List<Feedback> getAllFeedback() {
+        return em.createQuery("SELECT f FROM Feedback f", Feedback.class).getResultList();
+    }
+
+    public Feedback getFeedback(long id) throws NotFoundException {
+        Feedback feedback = em.find(Feedback.class, id);
+        if (feedback == null) throw new NotFoundException();
+        return feedback;
+    }
+
+    public void setResponded(long id, boolean responded) throws NotFoundException {
+        Feedback feedback = em.find(Feedback.class, id);
+        if (feedback == null) throw new NotFoundException();
+        feedback.setResponded(responded);
+        em.persist(feedback);
+        em.flush();
     }
 }
